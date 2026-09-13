2e73357a221a1048f225df65af9e4bee0ab4ffce
Eduardo Bozute <eduardobozutedev@gmail.com>
2026-09-13 17:25:24 -0300

    Add Gestures Enumeration

--- data_reading.h
+++ data_reading.h
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    UNMARKED_DATA,
+    HAND_AT_REST,
+    HAND_CLENCHED_IN_A_FIST,
+    WRIST_FLEXION,
+    WRIST_EXTENSION,
+    RADIAL_DEVIATIONS,
+    UNLNAR_DEVIATIONS,
+    EXTENDED_PALM
+} Gesture;
+
 int load_file_data(char[] file_name, double[][10] data_loaded);
 
 #endif
--- main.c
+++ main.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "data_reading.h"
 
 void main(){
 
